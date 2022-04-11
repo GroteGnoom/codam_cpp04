a999af4a9b09aa37d57796ea70143a9a3471005b
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-04-11 14:24:22 +0200

    dog -> animal

--- ex01/Animal.hpp
+++ ex01/Animal.hpp
@@ -8,8 +8,8 @@ class Animal {
 	public:
 		Animal();
 		virtual ~Animal();
-		Animal(const Animal& dog);
-		Animal &operator=(const Animal& dog);
+		Animal(const Animal& animal);
+		Animal &operator=(const Animal& animal);
 		virtual void makeSound() const;
 		std::string getType() const;
 		void setIdea(int index, std::string idea);
